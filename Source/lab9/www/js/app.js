f266a8edfc80d32e4306b1c92af5b3fe35bd278e
vyse8 <mattvyse8@gmail.com>
2015-11-05 13:51:58 -0600

    Lab 9
    Lab 9

---
+++ Source/lab9/www/js/app.js
@@ -0,0 +1,112 @@
+// Module Pattern, Constructor Pattern, Factory Pattern
+
+var App = angular.module('starter', ['ionic'])
+
+
+
+App.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+App.controller('AppCtrl', function ($scope, $http) {
+    $scope.register = function (username, password) {
+        $http({
+            method: 'POST',
+            url: 'https://api.mongolab.com/api/1/databases/testbeerdb/collections/users?apiKey=Sz-l6ovMdo7WLyI0yNSA8rZRMbN4vd6C',
+            data: JSON.stringify({
+                name: username,
+                password: password
+            }),
+            contentType: "application/json"
+        }).success(function () {})
+        alert(username + ' user added.')
+    }
+    $scope.login = function (username, password) {
+        $http.get('https://api.mongolab.com/api/1/databases/testbeerdb/collections/users?q={"name":"username","password":"password"}&apiKey=Sz-l6ovMdo7WLyI0yNSA8rZRMbN4vd6C')
+            .then(function successCallback(response) {
+                alert('Login Successful! Welcome ' + username + '.');
+                var input1 = document.getElementById('username').value;
+                localStorage.setItem('username', input1);
+                var input2 = document.getElementById('password').value;
+                localStorage.setItem('password', input2);
+                window.location = "Dashboard.html";
+            }, function errorCallback(response) {
+                $scope.loginErr = "Login Error."
+            });
+    }
+    $scope.updateMetrics = function (beerName, percentage) {
+        $http({
+            method: 'POST',
+            url: 'https://api.mongolab.com/api/1/databases/testbeerdb/collections/salesMetrics?apiKey=Sz-l6ovMdo7WLyI0yNSA8rZRMbN4vd6C',
+            data: JSON.stringify({
+                beerName: beerName,
+                percentage: percentage
+            }),
+            contentType: "application/json"
+        }).success(function () {})
+        alert(beerName + ' sales information has been added.')
+    }
+});
+
+function beerMetric( make, IBV, ABU ) {
+ 
+  this.make = make;
+  this.IBV = IBV;
+  this.ABU = ABU;
+ 
+  this.toString = function () {
+    return this.make + " has done " + this.IBV + " IBV";
+  };
+}
+ 
+var SamAdams = new beerMetric( "Samuel Adams", 15, 7 );
+var Boulevard = new beerMetric( "Boulevard", 14, 8);
+
+console.log( SamAdams.toString() );
+console.log( Boulevard.toString() );
+
+function beerFactory() {}
+beerFactory.prototype.ABU = ABU;
+ 
+beerFactory.prototype.ABU = function ( options ) {
+ 
+  switch(options.ABU){
+    case "ABU":
+      this.ABU = Light;
+      break;
+  }
+ 
+  return new this.ABU( options );
+ 
+};
+
+function loginUser1() {
+    window.plugins.toast.showLongBottom('Loading Dashboard', function (a) {
+        console.log('toast success: ' + a)
+    }, function (b) {
+        alert('toast error: ' + b)
+    })
+    var input1 = document.getElementById('username').value;
+    localStorage.setItem('username', input1);
+    var input2 = document.getElementById('password').value;
+    localStorage.setItem('password', input2);
+
+    window.location = "Dashboard.html";
+}
+
+
+
+function onLoad() {
+    var text = "";
+    text = localStorage.getItem("username");
+    document.getElementById('user').innerHTML = text;
+}
